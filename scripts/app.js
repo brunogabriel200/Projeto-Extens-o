3e89ecbf71595abbaeeb5d548b979e039aab84cd
Bruno Gabriel <168151070+brunogabriel200@users.noreply.github.com>
2024-11-25 02:06:33 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Validação de login
+    
     const loginForm = document.querySelector("form");
     if (loginForm) {
         loginForm.addEventListener("submit", (e) => {
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const email = document.getElementById("email").value;
             const password = document.getElementById("password").value;
 
-            if (email === "joao@email.com" && password === "123456") {
+            if (email === "bruno@email.com" && password === "123456") {
                 alert("Login bem-sucedido!");
-                // Certifique-se de que o caminho é correto
+                
                 window.location.href = "./livros.html"; 
             } else {
                 alert("Credenciais inválidas. Tente novamente.");
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    // Exibição de livros
+    
     const livrosContainer = document.getElementById("livros-container");
     if (livrosContainer) {
         const livros = [
